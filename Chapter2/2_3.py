884873732adf8c2432fdc8f1d5b0a726d272fb39
Anagah Joshi <anagha.elex@gmail.com>
2018-09-01 20:05:16 -0700

    added 2_3 solution

---
+++ Chapter2/2_3.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,data=0, next=None):
+        self.data = data
+        self.next = next
+
+
+def travese(head_node):
+    node = head_node
+    while node.next:
+        print(node.data, end=' -> ')
+        node = node.next
+    print(node.data)
+
+
+def remove_from_middle(rem_node):
+    rem_node.data = rem_node.next.data
+    rem_node.next = rem_node.next.next
+
+
+node1 = Node('A')
+node2 = Node('B')
+node3 = Node('C')
+node4 = Node('D')
+node5 = Node('E')
+
+basic_list_head = node1
+basic_list_head.next = node2
+basic_list_head.next.next = node3
+basic_list_head.next.next.next = node4
+basic_list_head.next.next.next.next = node5
+travese(basic_list_head)
+remove_from_middle(node3)
+travese(basic_list_head)

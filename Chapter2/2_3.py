b7cc6ff06f1f7078aeed9e3053244087ef537d09
Anagah Joshi <anagha.elex@gmail.com>
2018-09-01 20:06:52 -0700

    added 2_4 solution

--- Chapter2/2_3.py
+++ Chapter2/2_3.py
@@ -1,3 +1,13 @@
+"""
+Delete Middle Node: Implement an algorithm to delete a node in the middle (Le., any node but the first and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
+SOLUTION
+EXAMPLE
+Input: the node c from the linked list a- >b- >c - >d - >e- >f
+Result: nothing is returned, but the new linked list looks like a->b->d->e->f
+
+"""
+
+
 class Node:
     def __init__(self,data=0, next=None):
         self.data = data
